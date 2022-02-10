054ce482440ecc089379d423450032761c04b088
AAivliev <iaa.work@yandex.ru>
2022-02-10 23:44:52 +0300

    end creation getAll hobby api

--- src/main/kotlin/com/bikmay/hobi/rest/Application.kt
+++ src/main/kotlin/com/bikmay/hobi/rest/Application.kt
@@ -1,10 +1,16 @@
 package com.bikmay.hobi.rest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-class Application
+//@EnableJpaRepositories(basePackages = arrayOf("com.bikmay.*"))
+//@ComponentScan
+@EntityScan("com.bikmay.hobi.rest.repo.entity")
+open class Application
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
