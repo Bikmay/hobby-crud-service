91364d5a363d020db42b0d71b48389e72c73d2f2
AAivliev <iaa.work@yandex.ru>
2022-02-11 22:17:24 +0300

    fix for pagging load hobbys

--- src/main/kotlin/com/bikmay/hobi/rest/controller/HobbyController.kt
+++ src/main/kotlin/com/bikmay/hobi/rest/controller/HobbyController.kt
@@ -1,10 +1,12 @@
 package com.bikmay.hobi.rest.controller
 
+import com.bikmay.hobi.rest.dto.Count
 import com.bikmay.hobi.rest.repo.entity.HobbyEntity
 import com.bikmay.hobi.rest.service.HobbyService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hobby")
+@CrossOrigin
 class HobbyController(val service:HobbyService) {
     @GetMapping("/paging/{size}/{page}")
     fun getAll(@PathVariable("size") size:Int,@PathVariable("page") page:Int): Page<HobbyEntity> {
         return service.getAll(PageRequest.of(page,size));
+    }
+    @GetMapping("/count")
+    fun getCount():Count{
+        val response: Count = Count(service.getCount());
+        return response;
     }
 }
---
+++ src/main/kotlin/com/bikmay/hobi/rest/dto/Count.kt
@@ -0,0 +1,11 @@
+package com.bikmay.hobi.rest.dto
+
+import lombok.AllArgsConstructor
+import lombok.Getter
+import lombok.Setter
+
+@AllArgsConstructor
+@Setter
+@Getter
+class Count(val value:Long) {
+}
